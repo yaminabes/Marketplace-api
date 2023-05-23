1ea53eb011fc250c048a207a6ddfd72301532373
sofianeouardi <sofiane.ouardi@utbm.fr>
2023-05-23 18:54:24 +0200

    changement d'architecture

--- src/models/user.py
+++ src/models/user.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
-
 class User(Base):
     __tablename__ = "users"
 
@@ -13,14 +12,3 @@ class User(Base):
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
-
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
