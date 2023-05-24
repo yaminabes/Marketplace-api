60c2d6e00db6baa0de4f1e7a895450ed1bcd2dda
crimson <crimsonhatkrieg@protonmail.com>
2023-05-24 06:38:05 +0200

    Ajout frontend MUI WIP Sélection des annonces TODO Filter & databind

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Marketplace from './pages/Marketplace/Marketplace';
+
+function App() {
+  return (
+    <div className="App">
+      <Marketplace />
+    </div>
+  );
+}
+
+export default App;
