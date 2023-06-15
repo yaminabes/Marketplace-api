ec9654868c057c889fe9b525004870d8fb832411
crimson <crimsonhatkrieg@protonmail.com>
2023-06-15 07:05:20 +0200

    Correction MyCard

--- frontend/src/components/MyCard.js
+++ frontend/src/components/MyCard.js
@@ -9,23 +9,21 @@ import {CardActionArea} from "@mui/material";
 export default function MediaCard(props) {
     return (
         <Card sx={{...props.sx}}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="200"
-                    image={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.moneycrashers.com%2Fwp-content%2Fuploads%2F2019%2F03%2Ffind-good-babysitter.jpg&f=1&nofb=1&ipt=22c754cdf82932bbfe83c8937030f3bdc526af44ec2b9339284df8da9c8f601c&ipo=images'}
-                    title="market-add"
-                    alt="market-add"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.text}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+            <CardMedia
+                component="img"
+                height="200"
+                image={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.moneycrashers.com%2Fwp-content%2Fuploads%2F2019%2F03%2Ffind-good-babysitter.jpg&f=1&nofb=1&ipt=22c754cdf82932bbfe83c8937030f3bdc526af44ec2b9339284df8da9c8f601c&ipo=images'}
+                title="market-add"
+                alt="market-add"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.text}
+                </Typography>
+            </CardContent>
             <CardActions>
                 {props.renderCardActions ? (
                     props.renderCardActions
